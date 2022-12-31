58ba8ee8f7149584fc4b8d057c978a5c07456c4f
Michael Fudge <mafudge@syr.edu>
2022-12-31 16:57:34 -0500

    update to pybot client

--- __init__.py
+++ __init__.py
@@ -1,15 +1,79 @@
+import requests
 from IPython.core.magic import Magics, magics_class, cell_magic
 
-@magics_class
-class DemoMagics(Magics):
+from coursetools.nbenvironment import NbEnvironment
 
+import settings as settings
+
+
+def get_notebook_environment():
+    nbe_props = NbEnvironment().properties
+    skipkeys = ['minio_client', 'settings', 'run_datetime']
+    for k in skipkeys:
+        del nbe_props[k] 
+    return nbe_props
+
+def opt_status(eula=pbs.EULA):
+    if not os.path.exists(eula):
+        return "new"
+    else:
+        with open(eula,"r") as f:
+            opt = f.read().strip().lower()
+            return "in" if opt != 'out' else 'out'
+
+        
+@magics_class
+class MyMagics(Magics):
+
+    @cell_magic
+    def pybot(self, line, cell):
+        line = line.strip().lower()
+        opt = opt_status()
+        if opt=='new' or line in ['opt','help','?']:
+
+            def on_button_optin(b):
+                with open(pbs.EULA,"w") as f:
+                    f.write("in")
+                clear_output()
+                display(HTML("You are now opted IN. Run this cell again to use the assistant."))            
+
+            def on_button_optout(b):
+                with open(pbs.EULA,"w") as f:
+                    f.write("out")
+                clear_output()
+                display(HTML("You are now opted OUT. Run this cell again to use the assistant."))                            
+                
+            display(HTML(study_text))
+            if opt == 'in':
+                display(HTML("<p>At present, you have opted IN.<p>"))
+            elif opt=='out':
+                display(HTML("<p>At present, you have opted OUT.<p>"))
+            else:
+                display(HTML("<p>At present, you have not opted in or out.<p>"))
+                                
+            button_optin = ipywidgets.Button(description="Opt In", icon='fa-check')
+            button_optout = ipywidgets.Button(description="Opt Out", icon='fa-close')
+            display(ipywidgets.HBox([button_optin, button_optout]))            
+            button_optin.on_click(on_button_optin)
+            button_optout.on_click(on_button_optout)
+        else:  
+            # Run the main code 
+            lines = "".join(cell)
+            nbe_props = get_notebook_environment()
+            payload = { "prompt": line, "celldata": lines, "notebook_environment": nbe_props,"opt" : opt }
+            headers = { 'accept' : 'application/json', 'X-Api-Key': settings.API_KEY, 'Content-Type' : 'application/json' }
+            response = requests.post(settings.PYBOT_URL, headers=headers, json = payload)
+            response.raise_for_status()
+            output = response.text
+            self.shell.set_next_input(output, replace=False)
+               
     @cell_magic
     def demo(self, line, cell):
         self.shell.set_next_input(cell, replace=False)
-	
 
+        
 try:
     ip = get_ipython()
-    ip.register_magics(DemoMagics)
+    ip.register_magics(MyMagics)
 except NameError:
     pass # Not in a jupyter notebook
---
+++ settings.py
@@ -0,0 +1,36 @@
+API_KEY = "you68hgbe3o8gvic9np5agyyqr5jneqbspz8urge"
+PYBOT_URL = "https://pybot.pub.cent-su.org"
+EULA = "/home/jovyan/library/.pybot-eula"
+STUDY_TEXT = '''
+<h3>Welcome to pybot</h3>
+<p><b>Your AI Programming Assistant powered by OpenAI Codex.</b></p>
+<p>My name is Michael Fudge and I am a professor of practice at Syracuse University. <br>
+I am inviting you to participate in a research study for Spring 2023. I am interested in studying <br>
+<em>how AI programming assistants affect student learning in programming courses</em>, such as this one.</p>
+
+<p>This AI assistant can help you to write, understand and debug code. As with any tool <br>
+on the internet, data is collected while you use it. Before you can use this AI assistant, <br>
+I ask you whether or not the data collected by it may be used for research purposes. </p>
+
+<p>Involvement in the study is voluntary and will not impact your grade in the course. <br>
+This means you can choose whether to participate and that you may withdraw from the study <br>
+at any time without penalty. I will not have access to the research participation data until <br>
+after grades are submitted. If you have any questions, concerns or complaints about the research <br> 
+please contact me at <a href="mailto:mafudge@syr.edu">mafudge@syr.edu</a>.</p>
+
+<p>Whenever one works with email or the internet, there is always the risk of compromising privacy,  <br>
+confidentiality, and/or anonymity. Your confidentiality will be maintained to the degree permitted <br>
+by the technology being used. It is important for you to understand that no guarantees can be made <br>
+regarding the interception of data sent via the internet by third parties. </p>
+
+<p>If you are 18 years of age or older, click: 
+<ul>
+  <li><b>Opt-In</b> If you AGREE to partcipcate in the study, or  
+  <li><b>Opt-Out</b> If you DO NOT AGREEE to partcipcate in the study.
+</ul>  
+</p>
+
+<p>You may change your participation in the study at any time by typing:<br>
+<code>%%pybot opt</code> and pressing enter 3 times into an empty code cell.</p>
+
+'''
